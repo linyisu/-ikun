55a6ecbdf73a0dadf608c64a5383a5bf80fd4f62
mengh04 <mengh0412@hotmail.com>
2025-06-05 11:24:32 +0800

    重新写

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-import csv
-import re
-from spider.get_comments import getInfo, getComments
-
-def safe_filename(name):
-    # 替换掉 Windows 文件名不允许的字符
-    return re.sub(r'[\\/:*?"<>|]', '_', name)
+from dotenv import load_dotenv
+import os
 
 def main():
-    BV = input("请输入BV号：")
-    link = f'https://www.bilibili.com/video/{BV}'
-    oid, title = getInfo(BV, link)
-    print(title[:-14])
+    load_dotenv()
+    cookie = os.getenv("BILI_COOKIE")
+    if not cookie:
+        raise ValueError("未找到 .env 文件中的 BILI_COOKIE 变量，请检查配置。")
+    
+    bv = 'BV1qC4y1E7oU'  # 示例 BV 号
 
-    needCnt = 1000
-    total_count = 0
-    filename = f'{safe_filename(title[:-14])}_评论.csv'
-    with open(filename, mode='w+', newline='', encoding='utf-8-sig') as file:
-        csv_writer = csv.writer(file)
-        csv_writer.writerow(['序号', '用户ID', '用户名', '用户等级', '性别', '评论内容', '评论时间', '回复数', '点赞数', '个性签名'])
-        getComments(oid)
+    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
